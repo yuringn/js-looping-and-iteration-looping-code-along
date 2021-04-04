120d48add21d5014fce4837b1761c3e0e3c732d8
Yuri Nguyen <yuringuyen312@gmail.com>
2021-04-04 19:15:10 -0400

    commit tree 714e7d807c1adb48519a2b7ca33cf52b3af26679

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan' ]
+const event = "surprise "
+function writeCards (names, event){
+    const thankYou = [];
+    for (let i = 0; i < names.length; i ++) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);}
+    return thankYou;
+}
+
+
+
+
+function countDown(num){
+    while (num>=0){
+        console.log(num);
+        num-=1
+    }
+   
+}
